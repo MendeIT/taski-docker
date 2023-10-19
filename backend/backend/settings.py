aaee9857a0346f6a14a78753cb47355cd7a12ea3
Aldar Dordzhiev <aldardordzhiev@yahoo.com>
2023-10-19 15:33:38 +0300

    Add .env.example

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = (os.getenv('ALLOWED_HOSTS', 'Test host')).split()
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
